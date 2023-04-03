b2f13cdc1a22ec5d822bc641509f14e133eaa1c4
Dev-gojoy <32jongwoo@gmail.com>
2023-04-03 16:58:55 +0900

    first commit

---
+++ src/App copy.jsx
@@ -0,0 +1,89 @@
+<div class="px-4 pt-4 pb-5 py-0 lg:px-6 md:py-4 lg:py-10 flex flex-col 3xl:flex-row bg-[#ffffff] text-xs 3xl:text-sm text-textSecondary leading-normal font-normal">
+  <div class="hidden 3xl:flex justify-center items-center pr-10 border border-l-0 border-t-0 border-b-0 border-r-[1px] border-white/[0.2]"></div>
+  <div class="block 3xl:hidden"></div>
+  <div class="flex flex-col lg:flex-row w-full">
+    <div class="pb-[50px] 3xl:pb-0 pt-4 lg:pt-4 lg:pb-5 3xl:px-10 flex flex-col lg:flex-row lg:items-center lg:gap-6">
+      <div class="flex flex-col md:pt-0 md:pb-0 lg:items-left gap-1">
+        <div class="flex gap-3">
+          <span>작성자</span>
+          <span>고종우</span>
+        </div>
+        <div class="flex gap-3">
+          <span>연락처</span>
+          <span>010-1234-5678</span>
+        </div>
+      </div>
+      <div class="flex flex-col items-left gap-1">
+        <div class="flex gap-3">
+          <span>주소</span>
+          <span>서울 종로구 종로3길 17 디타워 16층</span>
+        </div>
+        <div class="flex gap-3">
+          <span>이메일</span>
+          <span>kjongwoo32@likelion.org </span>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 items-right mr-auto lg:mr-0 lg:ml-auto">
+      <div class="flex lg:ml-auto gap-4">
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+        <a
+          href="https://twitter.com/konkrit_io"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+        <a
+          href="https://blog.naver.com/modernlion"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+        <a
+          href="https://www.facebook.com/people/Konkrit/100085185777521/"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+        <a
+          href="https://www.youtube.com/channel/UC7wiiKUaXYHPeJyL1k_Wtmw"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+        <a
+          href="https://discord.com/invite/konkrit-originals"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+      </div>
+      <div class="flex items-center justify-center h-fit">
+        <div class="pr-2 lg:px-2 border border-t-0 border-b-0 border-l-0 border-r-0 border-white/[0.2]">
+          <a
+            href="/assets/KONKRIT_Terms_of_Service_2022.10.26.pdf"
+            target="_blank"
+          >
+            이용약관
+          </a>
+        </div>
+        <div class="px-2 border border-l-[1px] border-t-0 border-b-0 border-r-0 border-white/[0.2]">
+          <a
+            href="/assets/KONKRIT_Privacy_Policy_2022.11.14.pdf"
+            target="_blank"
+          >
+            개인정보처리방침
+          </a>
+        </div>
+        <div class="pl-2 border border-l-[1px] border-t-0 border-b-0 border-r-0 border-white/[0.2]">
+          <a
+            href="https://lion-modern.gitbook.io/konkrit-by-modern-lion/about/modern-lion"
+            target="_blank"
+            rel="noreferrer"
+          >
+            자주묻는질문
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>;
